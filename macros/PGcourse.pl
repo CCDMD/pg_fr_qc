75f55e36d510a2238ac33bac94be6d5bf799e9b7
Julie Tremblay <jtremblay@webwork.(none)>
2014-09-24 13:54:19 -0400

    Mise à jour des fichiers pg et ajout du fichier contextInequalitieFr.pl pour changer le or et le and pour un ou et un et

--- macros/PGcourse.pl
+++ macros/PGcourse.pl
@@ -1,4 +1,3 @@
-
 =head1 PGcourse.pl
 
  #
@@ -10,5 +9,25 @@
 =cut
 sub _PGcourse_init {};
 
+ $guest   = ($effectivePermissionLevel == -5) ;	####	practice user
+ 
+ ####	Presume that faculty observers are given TA status (permission 5).
+ $observe = ($effectivePermissionLevel >=  5) ;	####	TA or Professor (permission 10)
+ 
+ loadMacros( "problemRandomize.pl" ) ;
+ 
+ ####	Allow a Guest or Observer to always get a new version of problem.
+
+ ProblemRandomize( when => "Always", onlyAfterDue => 0, style => "Button", label => "Générer une nouvelle version de ce problème")
+ 	if	($guest or $observe) ;
+ 
+ ####	Allow everybody else (e.g., a student) to get a new version (for more practice)
+ ####	after correct answer(s) to first (scored) version OR after the assignment's due date
+ $when = (time >= $main::dueDate ? "Always" : "Correct");
+ ProblemRandomize( when => $when, onlyAfterDue => 0, style => "Button", label => "Générer une nouvelle version")
+ 	if	not ($guest or $observe) ;
+ 
 #loadMacros("source.pl");
+
+
 1;
